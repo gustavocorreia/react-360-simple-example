badb27f7540dfeeaa6bb903b570b83d05e0d3926
Gustavo Correia Santos <gustavocorreiasantos@outlook.com>
2019-06-10 22:55:03 -0300

    commit inicial

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 import {ReactInstance} from 'react-360-web';
+import SimpleRaycaster from 'simple-raycaster'
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -10,6 +11,11 @@ function init(bundle, parent, options = {}) {
     r360.createRoot('Hello360', { /* initial props */ }),
     r360.getDefaultSurface()
   );
+
+  r360.controls.clearRaycasters();
+  r360.controls.addRaycaster(SimpleRaycaster);
+
+  r360.controls.setBackground(r360.getAssetURL('moon.png'))
 }
 
 window.React360 = {init};
--- index.js
+++ index.js
@@ -3,16 +3,41 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  VrButton
 } from 'react-360';
 
 export default class Hello360 extends React.Component {
 
+  state = {
+    count: 0
+  }
+
+  _incrementCount = () => {
+    this.setState({ count: this.state.count++ })
+  }
+
+  _decreaseCount = () => {
+    if(this.setState.count > 0){
+      this.setState({ count: this.state.count-- })
+    }
+  }
+
+  // componentDidMount() {
+  //   setInterval(this._incrementCount, 1000);
+  // }
+
   render() {
     return (
       <View style={styles.panel}>
+        <VrButton onClick={this._incrementCount} style={ styles.greetingBox }>
+          <Text>+</Text>
+        </VrButton>
+        <VrButton onClick={this._decreaseCount} style={ styles.greetingBox }>
+          <Text>-</Text>
+        </VrButton>
         <Text style={styles.greeting}>
-            Simple Example with React 360
+          { `Count: ${ this.state.count }` }
         </Text>
       </View>
     );
@@ -23,9 +48,15 @@ const styles = StyleSheet.create({
   panel: {
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 0, 0, 1.0)',
+    backgroundColor: 'rgba(0, 0, 255, 5.0)',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#FFFFFF',
+    borderWidth: 2
   },
   greeting: {
     fontSize: 48,
